c47cbbf83de72c70ac7a9ea74c01583afc41d5d1
Weslley Rocha <48062297+WESLLEYROCHA87@users.noreply.github.com>
2021-06-22 09:54:38 -0300

    Estrutura finalizada
    Refeito o navbar, retirado o carousel.

--- includes/header.php
+++ includes/header.php
@@ -12,20 +12,27 @@
     <link rel="stylesheet" href="css/style.css">
     <!-- CSS Font -->
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="img/favicons/apple-touch-icon-152x152.png">
 
     <title>Weslley Rocha</title>
 
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top shadow p-0 mb-5 bg-dark">
-        <a class="navbar-brand float-xl-left mx-auto d-block" href="index.php"><img src="img/logo/logo_WR.png"
-                alt="Logo" width="90" title="Weslley Rocha"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdownMenuPrincipal"
-            aria-controls="conteudoNavbarSuportado" aria-expanded="false">
+
+    <!-- Navegador -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark shadow p-1 mb-5">
+        <div class="row">
+            <div class="col">
+                <a class="navbar-brand mx-auto d-block" href="index.php"><img src="img/logo/logo_WR.png" alt="Logo"
+                        width="90" title="Weslley Rocha"></a>
+            </div>
+        </div>
+        <button class="navbar-toggler mr-3" type="button" data-toggle="collapse"
+            data-target="#navbarDropdownMenuPrincipal" aria-controls="conteudoNavbarSuportado" aria-expanded="false">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarDropdownMenuPrincipal">
+        <div class="collapse navbar-collapse ml-3" id="navbarDropdownMenuPrincipal">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link text-light" href="#">Home <span class="sr-only">(página atual)</span></a>
@@ -36,15 +43,18 @@
                 <li class="nav-item">
                     <a class="nav-link text-light" href="#">Fale Conosco</a>
                 </li>
+
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown1" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Licitação</a>
+                    <div class="col col-md-4">
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
 
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                        <a class="dropdown-item " href="#">O que é?</a>
-                        <a class="dropdown-item " href="#">Como participar?</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item " href="#">Dúvidas Frequentes</a>
+                            <a class="dropdown-item" href="#">O que é?</a>
+                            <a class="dropdown-item" href="#">Como participar?</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Dúvidas Frequentes</a>
+                        </div>
                     </div>
                 </li>
             </ul>
@@ -58,11 +68,11 @@
                                         id="navbarDropdownMenuSecundario" role="button" data-toggle="dropdown"
                                         aria-haspopup="true" aria-expanded="false">Developer</a>
                                     <div class="dropdown-menu " aria-labelledby="navbarDropdownMenuSecundario"><a
-                                            class="dropdown-item" href="#">Em Desenvolvimento</a></div>
+                                            class="dropdown-item" href="#">Em Teste</a></div>
                                 </li>
                             </div>
                             <div>
-                                <a href="https://github.com/WESLLEYROCHA87"><img class="img-fluid"
+                                <a href="https://github.com/WESLLEYROCHA87" target="_blank"><img class="img-fluid"
                                         src="img/logo/GitHub-Mark-Light-32px.png" alt="" width="25"></a>
                             </div>
                         </div>
