6a9fb9cfa339caf08f36efde30f3215e836031ae
Weslley Rocha <48062297+WESLLEYROCHA87@users.noreply.github.com>
2021-06-22 14:53:19 -0300

    Feito Correções header criado footer

--- index.php
+++ index.php
@@ -1,6 +1,51 @@
 <?php include __DIR__ . '/includes/header.php'; ?>
 
+<div class="container">
 
+
+    <div class="row justify-content-center my-2">
+        <div class="col-12 col-sm-6 my-2 order-2">
+            <div class="card">
+                <img class="img-fluid" src="img/imgHome/img1.png" alt="">
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 my-2 order-1">
+            <div class="card">
+                <h1 class="justify-content-center">Texto Imagem 01</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-2">
+        <div class="col-12 col-sm-6 my-2 order-4">
+            <div class="card">
+                <h1 class="justify-content-center">Texto Imagem 2</h1>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 my-2 order-3">
+            <div class="card">
+                <img class="img-fluid" src="img/imgHome/img2.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-2">
+        <div class="col-12 col-sm-6 my-2 order-5">
+            <div class="card">
+                <h1 class="justify-content-center">Texto Imagem 3</h1>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 my-2 order-6">
+            <div class="card">
+                <img class="img-fluid" src="img/imgHome/img3.png" alt="">
+            </div>
+        </div>
+    </div>
+
+</div>
 
 
 
