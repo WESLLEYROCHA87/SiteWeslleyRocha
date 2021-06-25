1408b7d254c3048108ba27c2e96d554c70029c38
Weslley Rocha <48062297+WESLLEYROCHA87@users.noreply.github.com>
2021-06-25 08:48:30 -0300

    Atualizado pagina

--- faleconosco.php
+++ faleconosco.php
@@ -6,6 +6,22 @@
     <h1 class="text-center font-weight-light my-3">Fale Conosco</h1>
     <hr>
 
+    <!-- Mensagem de envio ou erro de mensagem -->
+    <?php if ($_SERVER['REQUEST_METHOD'] == 'POST') { ?>
+    <div class="alert alert-danger alert-dismissible fade show my-5" role="alert">
+        <?php echo 'Mensagem <strong>não pode ser enviada</strong>, favor entrar em contato através do' ?>
+
+
+        <a href="https://api.whatsapp.com/send?phone=5519974207424" target="_blank">
+            <strong class="text-success">Whatsapp!</strong></a>
+
+
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <?php } ?>
+
     <!-- Conteudo: Formulário -->
     <div id="contact-us">
         <form class="form-group my-5" action="faleconosco.php" method="POST">
@@ -39,7 +55,7 @@
             <div class="form-row">
                 <div class="form-group col-md-12">
                     <label for="mensagem">Escreva aqui</label>
-                    <textarea class="form-control" name="mensagem" rows="5"></textarea>
+                    <textarea class="form-control disabled" name="mensagem" rows="5" disabled></textarea>
                     </select>
                 </div>
             </div>
@@ -51,21 +67,7 @@
         </form>
     </div>
 
-    <!-- Mensagem de envio ou erro de mensagem -->
-    <?php if ($_SERVER['REQUEST_METHOD'] == 'POST') { ?>
-    <div class="alert alert-danger alert-dismissible fade show my-5" role="alert">
-        <?php echo 'Mensagem <strong>não pode ser enviada</strong>, favor entrar em contato através do' ?>
 
-
-        <a href="https://api.whatsapp.com/send?phone=5519974207424" target="_blank">
-            <strong class="text-success">Whatsapp!</strong></a>
-
-
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    <?php } ?>
 
     <!-- Conteudo: Contato Alternativo -->
     <h1 class="text-center font-weight-light">Estamos no Whatsapp:</h1>
