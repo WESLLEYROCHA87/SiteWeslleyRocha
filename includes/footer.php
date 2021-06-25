a24184ae0bfe66d55eaf2fb355a8de6a887b52b0
Weslley Rocha <48062297+WESLLEYROCHA87@users.noreply.github.com>
2021-06-25 09:32:27 -0300

    Inserido scrip whatsapp

--- includes/footer.php
+++ includes/footer.php
@@ -31,6 +31,31 @@
 </div>
 
 
+<!-- Início Botão Whatssap Site -->
+<script type="text/javascript">
+(function() {
+    var options = {
+        whatsapp: "+55(19)974207424", // WhatsApp number
+        call_to_action: "Fale Conosco Agora!", // Call to action
+        position: "right", // Position may be 'right' or 'left'
+    };
+    var proto = document.location.protocol,
+        host = "whatshelp.io",
+        url = proto + "//static." + host;
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function() {
+        WhWidgetSendButton.init(host, proto, options);
+    };
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+})();
+</script>
+<!-- Final Botão Whatssap Site -->
+
+
 
 
 
